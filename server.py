dc38d29ff21ce55604d1d2d0b4e47fb26e565a4b
Itamar O <itamarost@gmail.com>
2013-08-22 08:09:03 +0300

    added webcam functionality, with webcam RPC server.

---
+++ server.py
@@ -0,0 +1,62 @@
+import os
+import argparse
+from wsgiref.simple_server import make_server
+from webcam import WebCam
+
+parser = argparse.ArgumentParser(description=
+                                 'Home Control RPC auxiliary server.')
+parser.add_argument('--host', default='',
+                    help='Host name for WSGI server')
+parser.add_argument('--port', type=int, default=8001,
+                    help='Port for WSGI server')
+parser.add_argument('--debug', action='store_true',
+                    help='Enable debug server')
+args = parser.parse_args()
+
+cam = WebCam()
+cam.saveSnapshot()
+
+def application(
+        # environ points to a dictionary containing CGI like env-variables
+        # which is filled by the server for each received request
+        environ,
+        # start_response is a callback function supplied by the server
+        # which will be used to send the HTTP status and headers to the server
+        start_response):
+    
+    pathinfo = environ['PATH_INFO']
+    if '/webcam.png' == pathinfo:
+        status = '200 OK'
+        response_headers = [('Content-Type', 'image/png'),]
+        start_response(status, response_headers)
+        return open(cam.saveSnapshot(), 'rb').read()
+    elif '/favicon.ico' == pathinfo:
+        status = '404 Not Found'
+        response_body = 'Not Found'
+    elif args.debug:
+        status = '200 OK'
+        # Sorting and stringifying the environment key, value pairs
+        response_body = ['%s: %s' % (key, value)
+                        for key, value in sorted(environ.items())]
+        response_body = '\n'.join(response_body)
+    else:
+        status = '403 Forbidden'
+        response_body = 'Not Authorized'
+    response_headers = [('Content-Type', 'text/plain'),
+                        ('Content-Length', str(len(response_body)))]
+    start_response(status, response_headers)
+    return [response_body]
+
+if '__main__' == __name__:
+    # Instantiate the WSGI server.
+    # It will receive the request, pass it to the application
+    # and send the application's response to the client
+    httpd = make_server(
+       args.host,  # The host name.
+       args.port,  # A port number where to wait for the request.
+       application # Our application object name, in this case a function.
+       )
+    print "Serving HTTP on port %d..." % (args.port)
+
+    # Respond to requests until process is killed
+    httpd.serve_forever()
---
+++ webcam.py
@@ -0,0 +1,37 @@
+import sys
+import tempfile
+import time
+
+WIN = LINUX = False
+if 'win32' == sys.platform:
+    WIN = True
+    from VideoCapture import Device
+else:
+    LINUX = True
+    import subprocess
+    GST_COMMAND = 'gst-launch-0.10'
+
+class WebCam(object):
+    def __init__(self, outfilename=None):
+        if outfilename:
+            self.outfilename = outfilename
+        else:
+            outfile = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
+            self.outfilename = outfile.name
+            outfile.close()
+        if WIN:
+            self.cam = Device()
+        elif LINUX:
+            self.callargs = [GST_COMMAND, '-v', 'v4l2src', '!',
+                             #'video/x-raw-yuv,width=320,height=240,framerate=20/1', '!',
+                             'decodebin', '!', 'ffmpegcolorspace', '!', 'pngenc',
+                             '!', 'filesink', 'location="%s"' % (self.outfilename)]
+            
+    def saveSnapshot(self):
+        if WIN:
+            self.cam.saveSnapshot(self.outfilename)
+        elif LINUX:
+            if 0 != subprocess.call(self.callargs):
+                return False
+        return self.outfilename
+
