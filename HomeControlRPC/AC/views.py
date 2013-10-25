646d8c51d1652dbeadd3b46f520fa334f067564f
Itamar O <itamarost@gmail.com>
2013-10-25 17:25:11 +0300

    added debug print to AC send command view

--- HomeControlRPC/AC/views.py
+++ HomeControlRPC/AC/views.py
@@ -13,6 +13,7 @@ def send_command(request):
         # Try every available AcControl object to send the command,
         #  until the first one that succeeds
         for ac_control in AcControl.objects.all():
+            logger.debug('Attempting sendCommand with %s' % (ac_control))
             res = ac_control.sendCommand(request.GET)
             if u'Success' == res:
                 break
