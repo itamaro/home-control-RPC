3fbd9d27b9238c98bc0d83a5a54ec372814145b2
Itamar O <itamarost@gmail.com>
2013-10-26 19:58:21 +0300

    migrated cam RPC to Django app.
    added mocked image support.
    added support for multiple webcam definitions, returning the first successful one (by priority).

--- HomeControlRPC/cam/models.py
+++ HomeControlRPC/cam/models.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import tempfile
 import time
 import logging
@@ -11,31 +12,63 @@ WIN = LINUX = False
 if 'win32' == sys.platform:
     WIN = True
     from VideoCapture import Device
+    DEFAULT_DEV = '0'
 else:
     LINUX = True
     import subprocess
-    GST_COMMAND = 'gst-launch-0.10'
+    DEFAULT_DEV = 'gst-launch-0.10'
 
-class WebCam(object):
-    def __init__(self, outfilename=None):
-        if outfilename:
-            self.outfilename = outfilename
-        else:
-            outfile = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
-            self.outfilename = outfile.name
-            outfile.close()
-        if WIN:
-            self.cam = Device()
-        elif LINUX:
-            self.callargs = [GST_COMMAND, '-v', 'v4l2src', '!',
-                             #'video/x-raw-yuv,width=320,height=240,framerate=20/1', '!',
-                             'decodebin', '!', 'ffmpegcolorspace', '!', 'pngenc',
-                             '!', 'filesink', 'location="%s"' % (self.outfilename)]
-            
+def is_file_image(path):
+    return os.path.isfile(path) and   \
+           os.path.splitext(path)[-1].lower() in ('.png', '.jpg', '.gif')
+    
+def _get_tmp_filename():
+    tmpfile = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
+    tmpfile.close()
+    return tmpfile.name
+
+class WebCam(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    # The priority for using when sending commands (lower value = higher priority)
+    priority = models.PositiveIntegerField(default=1)
+    
+    # If an image file - will use as mocked response instead of actual webcam.
+    # In Windows, value to pass to VideoCapture.Device (as string).
+    # In Linux, gst-launch command name to execute for webcam capture.
+    webcam_source = models.CharField(max_length=255,
+                                     default=DEFAULT_DEV)
+                                     #default='cam/test-data/SumoNoona.png')
+    
+    # Where to store snapshots from the webcam.
+    #  Requires write access, obviously...
+    #  Leave blank to use randomly generated temporary files.
+    snapshot_file = models.CharField(max_length=255, blank=True, default='')
+    
+    def __unicode__(self):
+        return u'%s #%d' % (self.name, self.priority)
+    
     def saveSnapshot(self):
+        if is_file_image(self.webcam_source):
+            logger.debug('%s returning mock image file "%s"' %
+                         (self, self.webcam_source))
+            return self.webcam_source
+        snapshot_file = self.snapshot_file
+        if not snapshot_file:
+            snapshot_file = _get_tmp_filename()
+            logger.debug('Using temporary file "%s"' % (snapshot_file))
         if WIN:
-            self.cam.saveSnapshot(self.outfilename)
+            try:
+                cam = Device(int(self.webcam_source))
+                cam.saveSnapshot(snapshot_file)
+                time.sleep(1.0)
+                cam.saveSnapshot(snapshot_file)
+            except Exception:
+                return ''
         elif LINUX:
-            if 0 != subprocess.call(self.callargs):
-                return False
-        return self.outfilename
+            callargs = [self.webcam_source, '-v', 'v4l2src', '!',
+                        'decodebin', '!', 'ffmpegcolorspace', '!', 'pngenc',
+                        '!', 'filesink', 'location="%s"' % (snapshot_file)]
+            if 0 != subprocess.call(callargs):
+                return ''
+        return snapshot_file
--- HomeControlRPC/cam/views.py
+++ HomeControlRPC/cam/views.py
@@ -2,18 +2,29 @@ import logging
 import json
 import sys
 import tempfile
+from mimetypes import guess_type
+
 from django.http import HttpResponse
 from django.conf import settings
 
-from cam.models import WebCam
+from cam.models import WebCam, is_file_image
 
 logger = logging.getLogger(__name__)
 
-cam = None
-if 'cam' in settings.INSTALLED_APPS:
-    cam = WebCam()
-
 def get_snapshot(request):
     "Take a webcam snapshot and return it as HTTP response"
-    return HttpResponse(open(cam.saveSnapshot(), 'rb').read(),
-                        content_type='image/png')
+    content_type = 'application/json'
+    body = json.dumps('Failed snapshot')
+    # Try every available WebCam object to take the snapshot,
+    #  until the first one that succeeds
+    for cam in WebCam.objects.all().order_by('priority'):
+        logger.debug('Attempting saveSnapshot with %s' % (cam))
+        snapshot_file = cam.saveSnapshot()
+        if snapshot_file and is_file_image(snapshot_file):
+            content_type, _ = guess_type(snapshot_file)
+            if content_type:
+                body = open(snapshot_file, 'rb').read()
+                break
+            else:
+                content_type = 'application/json'
+    return HttpResponse(body, content_type=content_type)
